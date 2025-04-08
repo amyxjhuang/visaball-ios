8fbe9ce8da6f51b390e7103ddaaa050ea18aecfc
Amy Huang <amyxjhuang@berkeley.edu>
2025-04-07 23:26:50 -0400

    Add ML model

--- Visaball-YOLO-iOS/Visaball-YOLO-iOS/ViewController.swift
+++ Visaball-YOLO-iOS/Visaball-YOLO-iOS/ViewController.swift
@@ -4,10 +4,26 @@
 //
 //  Created by Amy Huang on 4/7/25.
 //
+import UIKit
+import AVFoundation
+import Vision
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    var bufferSize: CGSize = .zero
+    var inferenceTime: CFTimeInterval  = 0;
+    
+    private let session = AVCaptureSession()
+    
+    @IBOutlet weak var previewView: UIView!
+    var rootLayer: CALayer! = nil
+    private var previewLayer: AVCaptureVideoPreviewLayer! = nil
+    private var detectionLayer: CALayer! = nil
+    private var inferenceTimeLayer: CALayer! = nil
+    private var inferenceTimeBounds: CGRect! = nil
+    
+    private var requests = [VNRequest]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
