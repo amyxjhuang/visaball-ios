d1ea19d10c387afba6e777bbb413ad9c1bfb2082
Amy Huang <amyxjhuang@berkeley.edu>
2025-04-07 20:44:18 -0400

    Add starter files

---
+++ Visaball-YOLO-iOS/Visaball-YOLO-iOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Visaball-YOLO-iOS
+//
+//  Created by Amy Huang on 4/7/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
