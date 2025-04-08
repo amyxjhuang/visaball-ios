5e8ac770cd4af0d9d4e1db8aefbc345e32beb41e
Amy Huang <amyxjhuang@berkeley.edu>
2025-04-08 15:59:58 -0400

    Link view controller to outlet and device input setup

--- Visaball-YOLO-iOS/Visaball-YOLO-iOS/ViewController.swift
+++ Visaball-YOLO-iOS/Visaball-YOLO-iOS/ViewController.swift
@@ -3,12 +3,10 @@
 //  Visaball-YOLO-iOS
 //
 //  Created by Amy Huang on 4/7/25.
-//
+//  Reference: https://medium.com/better-programming/how-to-build-a-yolov5-object-detection-app-on-ios-39c8c77dfe58
 import UIKit
 import AVFoundation
 import Vision
-
-import Cocoa
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     var bufferSize: CGSize = .zero
@@ -27,8 +25,11 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupCapture()
+        setupOutput()
+        setupLayers()
+        try? setupVision()
+        session.startRunning()
     }
 
     override var representedObject: Any? {
@@ -37,6 +38,52 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         }
     }
 
-
+    func setupCapture() {
+           var deviceInput: AVCaptureDeviceInput!
+           let videoDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back).devices.first
+           do {
+               deviceInput = try AVCaptureDeviceInput(device: videoDevice!)
+           } catch {
+               print("Could not create video device input: \(error)")
+               return
+           }
+           
+           session.beginConfiguration()
+           session.sessionPreset = .vga640x480
+           
+           guard session.canAddInput(deviceInput) else {
+               print("Could not add video device input to the session")
+               session.commitConfiguration()
+               return
+           }
+           session.addInput(deviceInput)
+           
+           do {
+               try  videoDevice!.lockForConfiguration()
+               let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
+               bufferSize.width = CGFloat(dimensions.width)
+               bufferSize.height = CGFloat(dimensions.height)
+               videoDevice!.unlockForConfiguration()
+           } catch {
+               print(error)
+           }
+           session.commitConfiguration()
+       }
+       
+    func setupOutput() {
+           let videoDataOutput = AVCaptureVideoDataOutput()
+           let videoDataOutputQueue = DispatchQueue(label: "VideoDataOutput", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
+           
+           if session.canAddOutput(videoDataOutput) {
+               session.addOutput(videoDataOutput)
+               videoDataOutput.alwaysDiscardsLateVideoFrames = true
+               videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+               videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+           } else {
+               print("Could not add video data output to the session")
+               session.commitConfiguration()
+               return
+           }
+       }
 }
 
